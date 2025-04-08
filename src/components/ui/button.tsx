f7a46762003c3be3726da0d8c9fd675dd0ebaa39
Isaac Hermel <isaachermel@gmail.com>
2025-04-08 10:44:43 -0300

    fix: button icon size variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-12 lg:size-10",
+        icon: "size-8 lg:size-10",
       },
     },
     defaultVariants: {
