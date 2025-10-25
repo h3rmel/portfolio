f0a1ddad5dbd3ed57b115932f72464cf37e8690a
h3rmel <isaach3rmel@gmail.com>
2025-10-25 14:49:26 -0300

    chore: replace Kode Mono font with JetBrains Mono Variable in styles and components for improved typography

--- src/components/carrer-list.tsx
+++ src/components/carrer-list.tsx
@@ -11,11 +11,22 @@ export function CarrerList({
   return (
     <ul className="flex flex-col space-y-4">
       {experiences.map((experience) => (
-        <li key={experience.employer}>
-          <h3>{experience.employer}</h3>
-          <p>{experience.position}</p>
-          <p>{experience.year}</p>
-          <img className="size-16" src={experience.logoUrl} alt={experience.employer} />
+        <li
+          key={experience.employer}
+          className="flex justify-between items-start border border-b-3 p-4 rounded-lg"
+        >
+          <div className="flex items-center space-x-4">
+            <img
+              className="size-16 rounded-full p-0.5 border border-border"
+              src={experience.logoUrl}
+              alt={experience.employer}
+            />
+            <div className="flex flex-col">
+              <h3>{experience.employer}</h3>
+              <p className="text-sm text-muted-foreground">{experience.position}</p>
+            </div>
+          </div>
+          <p className="text-sm text-muted-foreground">{experience.year}</p>
         </li>
       ))}
     </ul>
